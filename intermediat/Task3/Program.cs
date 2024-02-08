8d151b14020c920125714ed23d89f9c07b03c971
Yulia <kap8437@gmail.com>
2024-02-08 23:49:28 +0600

    add task3

---
+++ intermediat/Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿void PrintArrayReverse(int[] array, int index)
+{
+    if (index == 34)
+    {
+        Console.WriteLine(array[index]);
+        return;
+    }
+    
+    Console.WriteLine(array[index]);
+    
+    PrintArrayReverse(array, index - 1);
+}
+
+int[] array = { 35, 21, 33, 14, 5 };
+
+PrintArrayReverse(array, array.Length - 1);
