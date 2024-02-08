3d68716a2d4df9c523d29992f62cb42546ddee48
Yulia <kap8437@gmail.com>
2024-02-08 20:30:08 +0600

    add task1

---
+++ intermediat/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿int M = 39;
+int N = 83;
+void RecursivePrint(int m, int n)
+{
+    if (m == n)
+    {
+        Console.Write(m);
+        Console.Write(' ');
+        return;
+    }
+    else
+    {
+        RecursivePrint(m + 1, n);
+        Console.Write(m);
+        Console.Write(' ');
+    }
+}
+RecursivePrint(M, N);
