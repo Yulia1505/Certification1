6a0160164162932526ed6485c2bd0c366f7b6c57
Yulia <kap8437@gmail.com>
2024-02-08 20:46:24 +0600

    add task2

---
+++ intermediat/Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿int m = 3;
+int n = 14;
+int FuncAckermann(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else
+    {
+        return FuncAckermann(m - 1, n + 1);
+    }
+}
+Console.WriteLine(FuncAckermann(m, n));
